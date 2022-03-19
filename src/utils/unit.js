db49eb21b07ef101c0756932f323ff91d390ca2d
Nico Thiebes <nico.thiebes@xing.com>
2022-03-19 20:24:49 +0100

    show stackable unit options

--- src/utils/unit.js
+++ src/utils/unit.js
@@ -2,8 +2,8 @@ export const getAllOptions = (
   { mounts, equipment, options, command, magic },
   asString
 ) => {
-  const allMounts = mounts
-    ? mounts.filter(({ active }) => active).map(({ name_de }) => name_de)
+  const allCommand = command
+    ? command.filter(({ active }) => active).map(({ name_de }) => name_de)
     : [];
   const allEquipment = equipment
     ? equipment.filter(({ active }) => active).map(({ name_de }) => name_de)
@@ -11,17 +11,23 @@ export const getAllOptions = (
   const allOptions = options
     ? options.filter(({ active }) => active).map(({ name_de }) => name_de)
     : [];
-  const allCommand = command
-    ? command.filter(({ active }) => active).map(({ name_de }) => name_de)
+  const allStackableOptions = options
+    ? options
+        .filter(({ stackableCount }) => stackableCount > 0)
+        .map(({ name_de, stackableCount }) => `${stackableCount} ${name_de}`)
+    : [];
+  const allMounts = mounts
+    ? mounts.filter(({ active }) => active).map(({ name_de }) => name_de)
     : [];
   const allMagicItems = magic?.items
     ? magic.items.map(({ name_de }) => name_de)
     : [];
   const allOptionsArray = [
-    ...allCommand,
     ...allEquipment,
-    ...allMounts,
     ...allOptions,
+    ...allStackableOptions,
+    ...allCommand,
+    ...allMounts,
     ...allMagicItems,
   ];
   const allOptionsString = allOptionsArray.join(", ");
