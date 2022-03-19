1de972140b66a6aa8c9596aba987b55e02514f5f
Nico Thiebes <nico.thiebes@xing.com>
2022-03-19 13:00:32 +0100

    add missing magic items

--- src/pages/magic/Magic.js
+++ src/pages/magic/Magic.js
@@ -18,7 +18,26 @@ const nameMap = {
   general: {
     name_de: "Magische Gegenstände",
   },
+  weapon: {
+    name_de: "Magische Waffen",
+  },
+  armor: {
+    name_de: "Magische Rüstungen",
+  },
+  talisman: {
+    name_de: "Talismane",
+  },
+  banner: {
+    name_de: "Magische Standarten",
+  },
+  artifact: {
+    name_de: "Arkane Artifakte",
+  },
+  "enchanted-item": {
+    name_de: "Verzauberte Gegenstände",
+  },
 };
+let prevItemType, isFirstItemType;
 
 const updateIds = (items) => {
   return items.map((item) => ({
@@ -136,31 +155,47 @@ export const Magic = ({ isMobile }) => {
         {items.map((item) => (
           <Fragment key={item.name_de}>
             <h2 className="unit__subline">{item.name_de}</h2>
-            {item.items.map((magicItem) => (
-              <div className="checkbox" key={magicItem.id}>
-                <input
-                  type="checkbox"
-                  id={`${item.id}-${magicItem.id}`}
-                  value={`${item.id}-${magicItem.id}`}
-                  onChange={(event) => handleMagicChange(event, magicItem)}
-                  checked={
-                    unit?.magic?.items
-                      ? unit.magic.items.find(
-                          ({ id }) => id === `${item.id}-${magicItem.id}`
-                        ) || false
-                      : false
-                  }
-                  className="checkbox__input"
-                />
-                <label
-                  htmlFor={`${item.id}-${magicItem.id}`}
-                  className="checkbox__label"
-                >
-                  {magicItem.name_de}
-                  <i className="checkbox__points">{`${magicItem.points} Pkte.`}</i>
-                </label>
-              </div>
-            ))}
+            {item.items.map((magicItem) => {
+              if (prevItemType !== magicItem.type) {
+                prevItemType = magicItem.type;
+                isFirstItemType = true;
+              } else {
+                isFirstItemType = false;
+              }
+
+              return (
+                <>
+                  {isFirstItemType && (
+                    <h3 className="magic__type">
+                      {nameMap[magicItem.type].name_de}
+                    </h3>
+                  )}
+                  <div className="checkbox" key={magicItem.id}>
+                    <input
+                      type="checkbox"
+                      id={`${item.id}-${magicItem.id}`}
+                      value={`${item.id}-${magicItem.id}`}
+                      onChange={(event) => handleMagicChange(event, magicItem)}
+                      checked={
+                        unit?.magic?.items
+                          ? unit.magic.items.find(
+                              ({ id }) => id === `${item.id}-${magicItem.id}`
+                            ) || false
+                          : false
+                      }
+                      className="checkbox__input"
+                    />
+                    <label
+                      htmlFor={`${item.id}-${magicItem.id}`}
+                      className="checkbox__label"
+                    >
+                      {magicItem.name_de}
+                      <i className="checkbox__points">{`${magicItem.points} Pkte.`}</i>
+                    </label>
+                  </div>
+                </>
+              );
+            })}
           </Fragment>
         ))}
       </MainComponent>
