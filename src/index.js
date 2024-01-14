72155ef173bf262756e0818f84790b8eb8c8d39c
Nico Thiebes <nico.thiebes@xing.com>
2024-01-14 14:27:49 +0100

    add sentry without replays

--- src/index.js
+++ src/index.js
@@ -30,9 +30,6 @@ Sentry.init({
   ],
   // Performance Monitoring
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
 const metaDescription = {
