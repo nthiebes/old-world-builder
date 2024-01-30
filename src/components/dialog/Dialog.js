d50346cf784764682095b2690bd2c54d7550efff
Nico Thiebes <nico.thiebes@xing.com>
2024-01-30 21:17:36 +0100

    add close button copy

--- src/components/dialog/Dialog.js
+++ src/components/dialog/Dialog.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect } from "react";
 import PropTypes from "prop-types";
+import { useIntl } from "react-intl";
 
 import { Button } from "../button";
 
@@ -7,6 +8,7 @@ import "./Dialog.css";
 
 export const Dialog = ({ open, onClose, children }) => {
   const dialogRef = useRef(null);
+  const intl = useIntl();
   const handleClose = () => {
     dialogRef.current.close();
     onClose();
@@ -25,7 +27,7 @@ export const Dialog = ({ open, onClose, children }) => {
         className="dialog__close"
         icon="close"
         type="text"
-        label="Close dialog"
+        label={intl.formatMessage({ id: "header.close" })}
         color="dark"
         onClick={handleClose}
       />
