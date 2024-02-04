6e599b63f5cf18fe0f45981b82cf696a9f54f166
Nico Thiebes <nico.thiebes@xing.com>
2024-02-04 11:30:48 +0100

    update changelog

--- src/pages/changelog/Changelog.js
+++ src/pages/changelog/Changelog.js
@@ -39,12 +39,46 @@ export const Changelog = () => {
 
         <hr />
 
+        <h3>v0.14</h3>
+        <p>
+          <time>
+            <i>
+              <FormattedDate
+                value={new Date("2024-02-04 11:11:11")}
+                month="long"
+                day="2-digit"
+                year="numeric"
+              />
+            </i>
+          </time>
+        </p>
+        <ul className="changelog__list">
+          <li>
+            <FormattedMessage
+              id="changelog.change26"
+              values={{
+                b: (msg) => <b>{msg}</b>,
+              }}
+            />
+          </li>
+          <li>
+            <FormattedMessage
+              id="changelog.change27"
+              values={{
+                b: (msg) => <b>{msg}</b>,
+              }}
+            />
+          </li>
+        </ul>
+
+        <hr />
+
         <h3>v0.13</h3>
         <p>
           <time>
             <i>
               <FormattedDate
-                value={new Date("2024-01-31 11:11:11")}
+                value={new Date("2024-02-02 11:11:11")}
                 month="long"
                 day="2-digit"
                 year="numeric"
