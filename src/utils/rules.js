ef23e6e9cd61c05d9d7346edf269ba93882b8a70
Daniel Trierweiler <daniel.trierweiler@posteo.de>
2025-03-23 16:17:52 +0100

    Added notes for the skaven and fixed the selection of grey seers

--- src/utils/rules.js
+++ src/utils/rules.js
@@ -3160,7 +3160,7 @@ export const rules = {
       maxPercent: 50,
       units: [
         {
-          ids: ["skaven-warlord", "grey-seer"],
+          ids: ["skaven-warlord"],
           min: 0,
           max: 1,
           requiresType: "all",
