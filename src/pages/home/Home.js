d282cc2bed13f307b59289e19480c106886d7d3a
Nico Thiebes <nico.thiebes@gmail.com>
2025-08-14 17:07:49 +0200

    more wip on folders

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,16 @@
-import { Fragment, useEffect } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { FormattedMessage, useIntl } from "react-intl";
 import { Helmet } from "react-helmet-async";
+import classNames from "classnames";
 
 import { Button } from "../../components/button";
 import { Icon } from "../../components/icon";
 import { ListItem, OrderableList } from "../../components/list";
 import { Header, Main } from "../../components/page";
+import { Dialog } from "../../components/dialog";
 import { getAllPoints } from "../../utils/points";
 import { useTimezone } from "../../utils/useTimezone";
 import { setArmy } from "../../state/army";
@@ -37,9 +39,9 @@ import fantasyweltEn from "../../assets/fantasywelt_en.jpg";
 import mwgForge from "../../assets/mwg-forge.gif";
 import { swap } from "../../utils/collection";
 import { useLanguage } from "../../utils/useLanguage";
-import { setLists, toggleFolder } from "../../state/lists";
+import { updateLocalList, updateListsFolder } from "../../utils/list";
+import { setLists, toggleFolder, updateList } from "../../state/lists";
 import { getRandomId } from "../../utils/id";
-import { updateListsFolder, updateLocalList } from "../../utils/list";
 
 import "./Home.css";
 
@@ -74,6 +76,9 @@ export const Home = ({ isMobile }) => {
   const { timezone } = useTimezone();
   const dispatch = useDispatch();
   const intl = useIntl();
+  const [dialogOpen, setDialogOpen] = useState(null);
+  const [activeMenu, setActiveMenu] = useState(false);
+  const [folderName, setFolderName] = useState("");
   const resetState = () => {
     dispatch(setArmy(null));
     dispatch(setItems(null));
@@ -88,6 +93,68 @@ export const Home = ({ isMobile }) => {
   };
   const folders = lists.filter((list) => list.type === "folder");
   const listsWithoutFolders = lists.filter((list) => list.type !== "folder");
+  const moreButtons = [
+    {
+      name: intl.formatMessage({
+        id: "misc.rename",
+      }),
+      icon: "edit",
+      callback: ({ name }) => {
+        setFolderName(name);
+        setDialogOpen("edit");
+      },
+    },
+    {
+      name: intl.formatMessage({
+        id: "misc.delete",
+      }),
+      icon: "delete",
+      callback: () => setDialogOpen("delete"),
+    },
+  ];
+  const handleCancelClick = (event) => {
+    event.preventDefault();
+    setDialogOpen(null);
+    setActiveMenu(null);
+    setFolderName("");
+  };
+  const handleDeleteConfirm = () => {
+    const newLists = updateListsFolder(
+      lists.filter((list) => list.id !== activeMenu)
+    );
+
+    setDialogOpen(null);
+    setActiveMenu(null);
+    dispatch(setLists(newLists));
+    localStorage.setItem("owb.lists", JSON.stringify(newLists));
+  };
+  const handleEditConfirm = () => {
+    const list = lists.find((list) => list.id === activeMenu);
+
+    setDialogOpen(null);
+    setActiveMenu(null);
+    dispatch(updateList({ ...list, listId: list.id, name: folderName }));
+    updateLocalList({
+      ...list,
+      name: folderName,
+    });
+  };
+  const handleNewConfirm = () => {
+    const newLists = updateListsFolder([
+      {
+        id: `folder-${getRandomId()}`,
+        name: folderName || "New folder",
+        type: "folder",
+        open: true,
+      },
+      ...lists,
+    ]);
+
+    localStorage.setItem("owb.lists", JSON.stringify(newLists));
+    dispatch(setLists(newLists));
+    setFolderName("");
+    setDialogOpen(null);
+  };
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -102,6 +169,110 @@ export const Home = ({ isMobile }) => {
         <link rel="canonical" href="https://old-world-builder.com/" />
       </Helmet>
 
+      <Dialog
+        open={dialogOpen === "delete"}
+        onClose={() => setDialogOpen(null)}
+      >
+        <p>
+          <FormattedMessage
+            id="editor.confirmDelete"
+            values={{
+              list: <b>{"list.name"}</b>,
+            }}
+          />
+        </p>
+        <div className="editor__delete-dialog">
+          <Button
+            type="text"
+            onClick={handleCancelClick}
+            icon="close"
+            spaceTop
+            color="dark"
+          >
+            <FormattedMessage id="misc.cancel" />
+          </Button>
+          <Button
+            type="primary"
+            submitButton
+            onClick={handleDeleteConfirm}
+            icon="delete"
+            spaceTop
+          >
+            <FormattedMessage id="misc.delete" />
+          </Button>
+        </div>
+      </Dialog>
+
+      <Dialog open={dialogOpen === "edit"} onClose={() => setDialogOpen(null)}>
+        <label htmlFor="folderName">
+          <FormattedMessage id="misc.folderName" />
+        </label>
+        <input
+          type="text"
+          id="folderName"
+          className="input"
+          value={folderName}
+          onChange={(e) => setFolderName(e.target.value)}
+          autoComplete="off"
+          maxLength="100"
+        />
+        <div className="editor__delete-dialog">
+          <Button
+            type="text"
+            onClick={handleCancelClick}
+            icon="close"
+            spaceTop
+            color="dark"
+          >
+            <FormattedMessage id="misc.cancel" />
+          </Button>
+          <Button
+            type="primary"
+            submitButton
+            onClick={handleEditConfirm}
+            icon="check"
+            spaceTop
+          >
+            <FormattedMessage id="misc.confirm" />
+          </Button>
+        </div>
+      </Dialog>
+
+      <Dialog open={dialogOpen === "new"} onClose={() => setDialogOpen(null)}>
+        <label htmlFor="newFolderName">
+          <FormattedMessage id="misc.folderName" />
+        </label>
+        <input
+          type="text"
+          id="newFolderName"
+          className="input"
+          value={folderName}
+          onChange={(e) => setFolderName(e.target.value)}
+          autoComplete="off"
+          maxLength="100"
+        />
+        <div className="editor__delete-dialog">
+          <Button
+            type="text"
+            onClick={handleCancelClick}
+            icon="close"
+            spaceTop
+            color="dark"
+          >
+            <FormattedMessage id="misc.cancel" />
+          </Button>
+          <Button
+            type="primary"
+            submitButton
+            onClick={handleNewConfirm}
+            icon="check"
+            spaceTop
+          >
+            <FormattedMessage id="misc.confirm" />
+          </Button>
+        </div>
+      </Dialog>
+
       {isMobile && <Header headline="Old World Builder" hasMainNavigation />}
       <MainComponent>
         {listsWithoutFolders.length > 0 && (
@@ -112,17 +283,7 @@ export const Home = ({ isMobile }) => {
               color="dark"
               icon="new-folder"
               onClick={() => {
-                const newLists = [
-                  {
-                    id: `folder-${getRandomId()}`,
-                    name: "New folder",
-                    type: "folder",
-                    open: true,
-                  },
-                  ...lists,
-                ];
-                localStorage.setItem("owb.lists", JSON.stringify(newLists));
-                dispatch(setLists(newLists));
+                setDialogOpen("new");
               }}
             />
           </section>
@@ -160,10 +321,34 @@ export const Home = ({ isMobile }) => {
               index
             ) =>
               type === "folder" ? (
-                <ListItem key={index} to="#" className="home__folder">
+                <ListItem
+                  key={index}
+                  to="#"
+                  className={classNames(
+                    "home__folder",
+                    activeMenu === id && "home__folder--active"
+                  )}
+                >
                   <span className="home__list-item">
-                    <h2 className="home__headline">
-                      <Icon className="" symbol="folder" />
+                    <h2 className="home__headline home__headline--folder">
+                      <Button
+                        type="text"
+                        label={intl.formatMessage({
+                          id: "export.optionsTitle",
+                        })}
+                        color="dark"
+                        icon="more"
+                        onClick={() => {
+                          if (activeMenu === id) {
+                            setActiveMenu(null);
+                          } else {
+                            setActiveMenu(id);
+                          }
+                        }}
+                        className={classNames(
+                          activeMenu === id && "header__more-button"
+                        )}
+                      />
                       <span className="home__folder-name">{name}</span>
                       <Button
                         type="text"
@@ -186,6 +371,29 @@ export const Home = ({ isMobile }) => {
                       />
                     </h2>
                   </span>
+                  {activeMenu === id && (
+                    <ul className="header__more folder__more">
+                      {moreButtons.map(
+                        ({
+                          callback,
+                          name: buttonName,
+                          icon,
+                          to: moreButtonTo,
+                        }) => (
+                          <li key={buttonName}>
+                            <Button
+                              type="text"
+                              onClick={() => callback({ name })}
+                              to={moreButtonTo}
+                              icon={icon}
+                            >
+                              {buttonName}
+                            </Button>
+                          </li>
+                        )
+                      )}
+                    </ul>
+                  )}
                 </ListItem>
               ) : (
                 <ListItem
@@ -198,6 +406,9 @@ export const Home = ({ isMobile }) => {
                       ?.open === false
                   }
                 >
+                  {folder ? (
+                    <Icon symbol="folder" className="home__folder-icon" />
+                  ) : null}
                   <span className="home__list-item">
                     <h2 className="home__headline">{name}</h2>
                     {description && (
