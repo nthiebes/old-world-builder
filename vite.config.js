57a0d7f0e58cc12a1375473c81de77fa224f376f
Nico Thiebes <nico.thiebes@gmail.com>
2026-03-09 10:16:41 +0100

    add vite sentry support

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "booky",
+      project: "old-world-builder",
+    }),
+  ],
   build: {
     outDir: "build",
+    sourcemap: true,
   },
 });
