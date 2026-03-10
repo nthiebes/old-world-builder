845e3f8d4bebcfebfb9b1609ad1f1aca4f5a6d12
Nico Thiebes <nico.thiebes@gmail.com>
2026-03-10 10:49:05 +0100

    don't count skaven detachments for min unit count

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -153,17 +153,6 @@ export const validateList = ({ list, language, intl }) => {
   const coreUnits = list?.core?.length
     ? list.core.filter(filterByTroopType).length
     : 0;
-  let coreUnitsDetachmentCount = 0;
-
-  if (list?.core?.length) {
-    list.core.forEach((unit) => {
-      if (unit.detachments && unit.detachments.length) {
-        coreUnitsDetachmentCount +=
-          unit.detachments.filter(filterByTroopType).length;
-      }
-    });
-  }
-
   const specialUnits = list?.special?.length
     ? list.special.filter(filterByTroopType).length
     : 0;
@@ -181,12 +170,7 @@ export const validateList = ({ list, language, intl }) => {
   const generalsCount = generals.length;
   const BSBsCount = BSBs.length;
   const nonCharactersCount =
-    coreUnits +
-    coreUnitsDetachmentCount +
-    specialUnits +
-    rareUnits +
-    mercUnits +
-    allyUnits;
+    coreUnits + specialUnits + rareUnits + mercUnits + allyUnits;
   const characterUnitsRules = rules[list.armyComposition]
     ? rules[list.armyComposition].characters.units
     : rules["grand-army"].characters.units;
